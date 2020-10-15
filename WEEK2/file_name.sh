b5f7480fae78cf767875889b9f95bbb91ca5e890
hemanthkumar7 <53857478+hemanthkumar7@users.noreply.github.com>
2020-10-15 17:10:08 +0530

    Update file_name.sh

--- WEEK2/file_name.sh
+++ WEEK2/file_name.sh
@@ -1,10 +1,9 @@
-#! bin/bash
-
-echo "Enter The File Name:"
-read f1
-echo "Permissions For $f1:"
-ls-1 $f1
-echo "Contents of $f1:"
-cat $f1
-echo "Word count of file is:"
-wc $f1
+#!/bin/sh
+echo "enter the filename"
+read filename
+echo "attributes:"
+ls -l $filename
+echo "contents:"
+cat $filename
+echo "word count:"
+wc -w $filename
