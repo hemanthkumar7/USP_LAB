904ca4ed81f25af1b43d2312297b008a8dc9a587
hemanthkumar7 <53857478+hemanthkumar7@users.noreply.github.com>
2020-11-05 01:41:25 -0800

    Add files via upload

---
+++ WEEK6/VOWEL/vowel.sh
@@ -0,0 +1,28 @@
+#finding char is vowel or not
+
+#! /bin/sh
+
+echo "Enter the alphabet"
+read char
+
+case $char in
+
+	'a') echo "Vowel"
+	;;
+
+	'e') echo "Vowel"
+	;;
+
+	'i') echo "Vowel"
+	;;
+
+	'o') echo "Vowel"
+	;;
+
+	'u') echo "Vowel"
+	;;
+
+	*) echo "Consonant"
+	;;
+
+esac
